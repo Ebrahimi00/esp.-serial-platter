646431001893db441544d57b4d52c8cd5cae0e3a
Reyhane Ebrahimi Khoi <121750125+Ebrahimi00@users.noreply.github.com>
2023-12-16 14:33:53 +0400

    Update ecg0.ino

--- ecg0.ino
+++ ecg0.ino
@@ -1,12 +1,9 @@
-#include <dummy.h>
-
 #include <driver/dac.h>
-#include <driver/adc.h>
 #include "esp_wifi.h"
 #include <Arduino.h>
-#include "SerialPlot.h"
 
-//اشاره گرهای تایمر
+
+//Timer pointers
 hw_timer_t *Timer0_Cfg = NULL;
 
 volatile int counter = 0;
@@ -16,7 +13,7 @@ float sine_value = sin(scaled_adc);
 
  // adc/16 - 12bits to 8bits
 
-//اجرای وقفه در زمانهای تعیین شده
+//Execution of interruptions at designated times
 void IRAM_ATTR Timer0_ISR() {
   dac_output_voltage(DAC_CHANNEL_1, adc);
   counter++;
@@ -25,15 +22,15 @@ void IRAM_ATTR Timer0_ISR() {
 void setup() {
   Serial.begin(9600);
   Serial.println("\nESP32 DAC sine wave");
-//تنظیم زمان وقفه           
+//Set timeout           
   Timer0_Cfg = timerBegin(0, 400, true);
   timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
   timerAlarmWrite(Timer0_Cfg, 10, true);
   timerAlarmEnable(Timer0_Cfg);
   // Enable DAC1 Channel's Output
   dac_output_enable(DAC_CHANNEL_1);
-  analogSetWidth(12);    // تنظیم رزولیشن 12
-  analogSetClockDiv(255);  // تبدیل سریعترین زمان
+  analogSetWidth(12);    //Setting the resolution 12
+  analogSetClockDiv(255);  // Fastest conversion time
     esp_wifi_stop();
 }
 
